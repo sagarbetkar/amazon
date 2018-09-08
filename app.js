e8abaa13b0e9f2e00f7128b334c22df1b7e25ef5
Sagar Betkar <sagarbetkar1994@gmail.com>
2018-09-08 22:10:16 +0530

    Added users api

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+/**
+ * Controllers (route handlers).
+ */
+ const userController = require('./controllers/users');
 
 var app = express();
 
@@ -17,4 +23,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+mongoose.connect('mongodb://localhost:27017/amazon');
+mongoose.connection.on('error', (error) => console.error(error));
+mongoose.connection.on('open', () => console.log("success in connecting to mongodb"));
+
+app.post('/api/v1/users', userController.postNewUser);
+app.get('/api/v1/users', userController.getAllUsers);
+app.get('/api/v1/users/:id', userController.getUserById);
+app.put('/api/v1/users/:id', userController.updateUserById);
+app.delete('/api/v1/users/:id', userController.deleteUserById);
+
+
 module.exports = app;
+
+app.listen(2611, () => console.log('Express server at 2611'));
